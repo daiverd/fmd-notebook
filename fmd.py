d3cd5edda22e0ae6cecd6e5c58098598c016ef84
david <david@rustytelephone.net>
2024-04-05 14:47:59 -0700

    Separate view into template file, added edit template

--- fmd.py
+++ fmd.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template_string 
+from flask import Flask, request, render_template, render_template_string 
 from pathlib import Path
 import markdown 
 import os 
@@ -8,9 +8,22 @@ base_dir = Path(Path(__file__).parent, "markdown_files")
 app = Flask(__name__) 
 
 # Function to get the list of markdown files 
-def get_markdown_files(): 
-    md_files = [f for f in os.listdir("markdown_files") if f.endswith('.md')] 
-    return md_files 
+def get_markdown_files(directory=""):
+  """
+  Retrieves a glob object containing markdown files from the specified directory using pathlib.
+  """
+
+  dir_path = Path(base_dir, directory)
+
+  # Handle potential errors during path construction
+  if not is_safe_path(dir_path):
+    raise ValueError(str(dir_path))
+
+  if not dir_path.is_absolute():
+    raise ValueError("Directory path must be absolute")
+
+  # Use path.glob to find all '.md' files (returns a generator)
+  return [path.relative_to(base_dir) for path in dir_path.glob("*.md")]
 
 def is_safe_path(path):
   """
@@ -26,7 +39,7 @@ def is_safe_path(path):
     raise ValueError("Input path must be absolute")
 
   # Check if the resolved path is within the base directory
-  return path_obj.is_relative_to(base_dir)
+  return path_obj == base_dir or path_obj.is_relative_to(base_dir)
 
 @app.route('/') 
 def index(): 
@@ -53,10 +66,22 @@ def view_markdown_file(filename):
     else: 
         content = "Markdown file not found!" 
          # Use a simple HTML template and insert the HTML content 
-    template = """ 
-    <!DOCTYPE html>     <html>     <head>         <title>Markdown Viewer</title>     </head> 
-    <body>         {{ content | safe }}     </body>     </html>     """ 
-    return render_template_string(template, content=content) 
+    return render_template("view.html", content=content, filename=filename) 
+
+@app.route('/edit/<path:filename>') 
+def edit_markdown_file(filename): 
+    content = "File not found." 
+    # Construct file path 
+    file_path = Path(base_dir, filename)
+         # Check if file exists 
+    if is_safe_path(file_path) and os.path.isfile(file_path): 
+        # Read the Markdown file 
+        with open(file_path, 'r') as f: 
+            content = f.read() 
+    else: 
+        content = "# new file" 
+         # Use a simple HTML template and insert the HTML content 
+    return render_template("edit.html", content=content, filename=filename) 
 
 if __name__ == '__main__': 
     app.run(debug=True)
